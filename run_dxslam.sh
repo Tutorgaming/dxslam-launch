6f23b0eb09bf6dbc638f4c424ed7943cf7609c10
Theppasith N <theppasith.n@obodroid.com>
2021-03-26 15:24:25 +0700

    Store the Launch File for launching up dxslam

---
+++ run_dxslam.sh
@@ -0,0 +1,2 @@
+source ~/ros/dxslam_ws/devel/setup.bash
+roslaunch dxslam_ros rgbd.launch
---
+++ run_feature.sh
@@ -0,0 +1,2 @@
+source ~/ros/vision_ws/devel/setup.bash
+roslaunch feature_extraction feature_extraction.launch
